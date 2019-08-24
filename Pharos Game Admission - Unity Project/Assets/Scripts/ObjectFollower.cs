019366990260566551ba5d258c92fe7679f87075
fr4ct1ons <gabriellucena23@gmail.com>
2019-08-23 22:31:18 -0300

    Added ObjectFollower to main camera

---
+++ Pharos Game Admission - Unity Project/Assets/Scripts/ObjectFollower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollower : MonoBehaviour
+{
+    [SerializeField] GameObject objectToFollow;
+    [SerializeField] bool followXPos = false, followYPos = false, followZPos = false;
+    [SerializeField] float xOffset = 0.0f, yOffset = 0.0f, zOffset = 0.0f;
+
+    Vector3 bufferVector = new Vector3();
+
+    private void Awake()
+    {
+        bufferVector = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(objectToFollow)
+        {
+            if (followXPos)
+                bufferVector.x = objectToFollow.transform.position.x + xOffset;
+            if(followYPos)
+                bufferVector.y = objectToFollow.transform.position.y + yOffset;
+            if (followZPos)
+                bufferVector.z = objectToFollow.transform.position.z + zOffset;
+
+            transform.position = bufferVector;
+        }
+    }
+}
